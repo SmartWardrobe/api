ae67a1d7dfd926bbaeb5d3e34eb97da036052484
Tugce Cetinkaya <tugce.ctky@gmail.com>
2017-11-23 11:53:58 +0300

    Create home.ts

---
+++ ionic/home.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {Http,Headers} from '@angular/http';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+ data:any = {};
+ 
+ constructor(public navCtrl: NavController, public http: Http){
+ this.data.username = '';
+ this.data.response = '';
+
+ this.http = http;
+ }
+
+ submit() {
+ var link = 'http://localhost:5000/api/user/';
+ var mydata = JSON.stringfy({username: this.data.username});
+
+ this.http.post(link, mydata)
+  .subscribe(data => {
+      this.data.response = data["_body"];
+
+  }, error => {
+
+  console.log("Oooops!");
+  })
+
+
+  }
+}
