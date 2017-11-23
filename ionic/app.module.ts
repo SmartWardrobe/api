70af6e7e9b06a7c5dd98f828ebc45300348cda5f
Tugce Cetinkaya <tugce.ctky@gmail.com>
2017-11-23 12:00:42 +0300

    Update app.module.ts

--- ionic/app.module.ts
+++ ionic/app.module.ts
@@ -1,4 +1,4 @@
-/src/app
+/src/app (src klasörünün app kısmında)
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
