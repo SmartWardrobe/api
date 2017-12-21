6b4c85d54e87ce619cb86d398ed0646fdedafe46
pleycpl <h4rvey@protonmail.com>
2017-12-21 23:49:49 +0000

    bazi fonksiyonlar util.py tasindi

--- Util.py
+++ Util.py
@@ -1,5 +1,15 @@
-
+import re
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def is_email_address_valid(email):
+    """Validate the email address using a regex."""
+    if not re.match("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", email):
+        return False
+    return True
+
+def hash(password):
+    """ return hashed password """
+    return password
