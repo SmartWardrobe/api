05723e662fbf564b9d15b5fa992c8b249f14efaa
pleycpl <h4rvey@protonmail.com>
2017-12-23 20:52:09 +0000

    python standart'ina cevrildi.

--- AwsOps.py
+++ AwsOps.py
@@ -7,19 +7,19 @@ BUCKET_NAME = os.environ.get("BUCKET_NAME")
 ACCESS_KEY_ID = os.environ.get("ACCESS_KEY_ID")
 ACCESS_SECRET_KEY = os.environ.get("ACCESS_SECRET_KEY")
 
-s3 = boto3.resource(
-        's3',
-        aws_access_key_id=ACCESS_KEY_ID,
-        aws_secret_access_key=ACCESS_SECRET_KEY,
-        config=Config(signature_version='s3v4')
+S3 = boto3.resource(
+    's3',
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    config=Config(signature_version='s3v4')
 )
 
 def upload_pic_to_s3_bucket(data, filename):
-    s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=data)
+    S3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=data)
 
 def download_pic_in_s3_bucket(filename):
     try:
-        s3.Bucket(BUCKET_NAME).download_file(filename, 'uploads/' + filename)
+        S3.Bucket(BUCKET_NAME).download_file(filename, 'uploads/' + filename)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("The object does not exist.")
@@ -28,19 +28,19 @@ def download_pic_in_s3_bucket(filename):
             raise
 
 def get_bucket_list_in_s3():
-    bucketNames = []
+    bucket_names = []
     s3client = boto3.client('s3')
     response = s3client.list_buckets()
     for bucket in response["Buckets"]:
-        bucketNames.append(bucket['Name'])
+        bucket_names.append(bucket['Name'])
 
-    return bucketNames
+    return bucket_names
 
 def get_file_list_in_s3_bucket():
-    ObjectList = []
+    object_list = []
     s3client = boto3.client('s3')
     theobjects = s3client.list_objects_v2(Bucket=BUCKET_NAME)
     for object in theobjects["Contents"]:
-        ObjectList.append(object["Key"])
+        object_list.append(object["Key"])
 
-    return ObjectList
+    return object_list
